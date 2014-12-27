c7cbf8d94d2897820468b10db60e1f19f2ebbcfa
Brent Vatne <brent.vatne@madriska.com>
2014-12-26 21:06:27 -0800

    Better display of "Shows to watch" in the popup

--- frontend/www/js/controllers/liked_shows_ctrl.js
+++ frontend/www/js/controllers/liked_shows_ctrl.js
@@ -1,6 +1,23 @@
 (function() {
   var LikedShowsCtrl = function($scope, Store, Actions) {
     Store.bindState($scope, function() { $scope.shows = Store.getLikedShows(); });
+
+    $scope.isFirstShow = function(i) {
+      if (i == 0) {
+        return true;
+      }
+    }
+
+    $scope.isLastShow = function(i) {
+      if (i == $scope.shows.length - 1) {
+        console.log("yup it's the last one!");
+        return true;
+      }
+    }
+
+    $scope.rowHeight = function(i) {
+      return $scope.isLastShow(i) ? '160px' : '90px'
+    }
   }
 
   angular.module('app').
